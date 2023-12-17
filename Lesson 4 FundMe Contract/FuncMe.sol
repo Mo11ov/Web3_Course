8877e4619a34522b4cc03846e0811754baddb57a
Rashko Mollov <rashko.1986@gmail.com>
2023-12-17 13:25:06 +0200

    Refactored the code

--- Lesson 4 FundMe Contract/FuncMe.sol
+++ Lesson 4 FundMe Contract/FuncMe.sol
@@ -9,19 +9,57 @@ contract FundMe {
     // this attaches PriceConverter library for all uint256 types and now they can access all func inside that lib
     using PriceConverter for uint256;
 
-    uint256 public minSumInUsd = 5e18; // 5 * (1 * 000 000 000 000 000 000)
+    uint256 public constant MINIMUM_USD = 5e18; // 5 * (1 * 000 000 000 000 000 000)
+
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
+
+    address public immutable i_owner;
+
+    constructor() {
+        // sets owner of Contract so funcs can be called only by the owner!!!
+        i_owner = msg.sender;
+    }
 
     function fund() public payable {
         require(
             // conversionRate func is from PriceConverter lib
-            msg.value.getConversionRate() >= minSumInUsd,
+            msg.value.getConversionRate() >= MINIMUM_USD,
             "You need to spend at least 5$ worth of ETH!!!"
         );
         funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] =
-            addressToAmountFunded[msg.sender] +
-            msg.value;
+        addressToAmountFunded[msg.sender] += msg.value;
+    }
+
+    function withDraw() public onlyOwner {
+        for (uint256 idx = 0; idx < funders.length; idx++) {
+            address funder = funders[idx];
+            addressToAmountFunded[funder] = 0;
+        }
+
+        // reseting funders array
+        funders = new address[](0);
+
+        // actually withdraw funds
+
+        // transfer - will throws error if more than 2300 gas is used and reverts tnx
+        // msg.sender = address
+        // payable(msg.sender) = payable address
+        // payable(msg.sender).transfer(address(this).balance);
+
+        // send - will return a bool if tnx is successfull
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed"); // reverts
+
+        // call = lower lvl function - returns bool and data
+        // (bool callSuccess, bytes memory dataReturned) = payable(msg.sender).call{value: address(this).balance}("");
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == i_owner, "Must be owner to call this func");
+        // adds rest of the modified function
+        _;
     }
 }
